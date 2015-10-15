df12d91633a1188553e6ce790cbbbe34149367ba
Garrett Berg <googberg@gmail.com>
2015-10-15 13:29:59 -0600

    new and better

--- litevault.py
+++ litevault.py
@@ -22,34 +22,43 @@ if sys.version_info[0] == 2:
 
 # Global current stored password
 curpass = ''
+keypress_delay_us = '10000'
 
 ##################################################
 # Constants
 
 pid_file = '/tmp/litevault.pid'
-intro_msg = 'litevault started. Type "?" or "h" for help'
-help_msg = '''
+intro_msg = r'''
+ ************************************
+ *       /-------\  Type "?" or "h" *
+ *  lite | vault |     for help.    *
+ *       \-------/                  *
+ ************************************'''
+help_msg = '''\
 Help:
 h or ?         = help
-q or exit      = quit
-s or save      = save vault state to file
-n              = set new password for vault (and save it)
-NONMATCHING [] = get all of item (same as 'a')
+q or exit      = quit litevault
+L              = lock litevault (same as if it timed out)
+s or save      = save vault state to disk)
+n              = set new password for vault (and save it to disk)
+t              = set new timeout password
 l or ls     [] = list items. Add an item to do a fuzzy search
+c or mk     [] = create/overwrite item in vault  * (in memory. Use 's' to save changes to file)
+d or rm     [] = delete data from vault          * (in memory, use 's' to save changes to file)
 u           [] = get user
 p           [] = get password
 i           [] = get info
-a           [] = get both user and password (insert a TAB between them and end with Return)
-c           [] = create/overwrite item in vault  * (in memory. Use 's' to save changes to file)
-d or rm     [] = delete data from vault          * (in memory, use 's' to save changes to file)
+a           [] = get user + password and show info
+[NON-MATCHING] = same as "a [NON-MATCHING]". Happens when item doesn't match any commands
 '''
+TIMEOUT_PWD_KEY='__litevault_timeout_password__'
 
 
 ##################################################
-# xdotool mappings
+# X keboard automation mappings
 # http://www.linux.org/threads/xdotool-keyboard.6414/
 
-xdotool_char_mappings = {
+x_char_mappings = {
     '~': 'asciitilde'   , '`': 'quoteleft'   , '!': 'exclam'       , '@': 'at'          ,
     '#': 'numbersign'   , '$': 'dollar'      , '%': 'percent'      , '^': 'asciicircum' ,
     '&': 'ampersand'    , '*': 'asterisk'    , '(': 'parenleft'    , ')': 'parenright'  ,
@@ -62,9 +71,25 @@ xdotool_char_mappings = {
 }
 
 
+def send_keypresses(characters, delay_us=10000):
+    '''Send a sequence of keypresses to the keyboard.
+
+    This function is intended to be secure -- as in outside processes should not be
+    able to see what it is doing.
+
+    :characters: characters to send
+    :delay_us: delay in microseconds between each keypress
+    '''
+    k = '\nusleep {}\nkey '.format(delay_us)
+    keypresses = (c if c not in x_char_mappings else x_char_mappings[c]
+                  for c in characters)
+    keypresses = ('key ' + k.join(keypresses) + '\n').encode()
+    sh = subprocess.Popen(['xte'], stdin=subprocess.PIPE)
+    sh.communicate(input=keypresses)
+
+
 ##################################################
 # Convenience Functions
-
 
 def set_password_dialog(first_message="New Password: "):
     while True:
@@ -78,8 +103,10 @@ def set_password_dialog(first_message="New Password: "):
         else:
             print("Passwords don't match. Retrying...")
 
+
 def print_item_not_found(item):
     print("Item not found {}".format(item))
+
 
 def pplist(l, cols=4, indent=0):
     if not isinstance(l, list):
@@ -93,12 +120,12 @@ def pplist(l, cols=4, indent=0):
         print(' ' * indent + "".join(str.ljust(i, 20) for i in row))
 
 
-def clear_screen(msg=True):
+def clear_screen(wait=True):
     out = ''
-    if msg:
-        if msg is True:
-            msg = '\n ** Press ENTER to clear the screen **'
-        out = input(msg)
+    if wait:
+        if wait is True:
+            wait = '\n ** Press ENTER to clear the screen **'
+        out = input(wait)
     print('\x1b[0;0H\x1b[2J\n' + intro_msg)
     return out
 
@@ -107,23 +134,18 @@ def output_password(signum, stack):
     '''Output the password on the keyboard'''
     if not curpass:
         return
-    k = ' key '
-    characters = [c if c not in xdotool_char_mappings else xdotool_char_mappings[c]
-                  for c in curpass]
-    args = k + k.join(characters)
-    subprocess.check_call(('xdotool ' + args).encode().split())
+    send_keypresses(curpass, keypress_delay_us)
 
 
 def verify_environment():
     requirements = {
-        "Xorg's xdotool": 'xdotool --version',
+        "xautomation: need xte": 'xte -h',
     }
     for r, cmd in requirements.items():
         try:
-            subprocess.check_output(cmd.split())
+            subprocess.check_output(cmd.encode().split())
         except Exception as E:
-            print('Dependency not met: {}'.format(r))
-            sys.exit(1)
+            quit_app('!! Dependency not met: {} !!'.format(r), rc=1)
 
 
 def input_eof(msg=''):
@@ -143,8 +165,14 @@ def generate_password(length):
     random.seed = (os.urandom(1024))
     return ''.join(random.choice(chars) for i in range(length))
 
+
+def quit_app(msg=None, rc=0):
+    print(msg or " ** Quitting litevault **")
+    sys.exit(rc)
+
+
 ##################################################
-# Conveinience Functions
+# Vault Class
 
 class Vault(dict):
     """Safe password storage and retrieval"""
@@ -169,7 +197,7 @@ class Vault(dict):
             return
         with open(self.path, 'rb') as f:
             encrypted = f.read()
-        text = scrypt.decrypt(encrypted, self.password, maxtime=self.maxtime * 3)
+        text = scrypt.decrypt(encrypted, self.password, maxtime=self.maxtime * 20)
         data = json.loads(text)
         self.clear()
         self.update(data)
@@ -190,7 +218,6 @@ class Vault(dict):
 ##################################################
 # User Functions
 
-##################################################
 # Loading and Viewing
 
 def load_info(vault, item):
@@ -266,7 +293,6 @@ def list_items(vault, item=None):
     execute_command(vault, user_input)
 
 
-##################################################
 # Creating, Deleting, and Saving
 
 def create_item(vault, item):
@@ -326,6 +352,7 @@ def create_item(vault, item):
         print(" ** Loading new item's password **")
         load_password(vault, item)
 
+
 def delete_item(vault, item):
     if item not in vault:
         print("Item {} not in vault".format(item))
@@ -343,7 +370,7 @@ def save_vault(vault, *args):
 
 
 def new_password(vault, *args):
-    newpass = set_password_dialog()
+    newpass = set_password_dialog("Enter new vault password: ")
     if not newpass:
         print("No password entered, skipping")
         return
@@ -353,32 +380,35 @@ def new_password(vault, *args):
     print("Vault with new password saved")
 
 
+def set_timeout_pwd(vault, *args):
+    tpass = set_password_dialog("Enter new timeout password: ")
+    if not tpass:
+        print("No password entered, doing nothing.")
+        return
+    vault[TIMEOUT_PWD_KEY] = tpass
+    print("New timeout password saved")
 
 
-##################################################
 # Other User Functions
-
-def clear_password():
-    global curpass
-    curpass = ''
-    clear_screen(False)
-
 
 def execute_command(vault, user_input):
     print_help = lambda vault, item: print(help_msg)
-    quit = lambda vault, item: sys.exit(0)
     interface = {
+        # global actions
         '?': print_help,    'h': print_help,
-        'q': quit,          'exit': quit,
+        'q': quit_app,      'exit': quit_app,
+        's': save_vault,    'save': save_vault,
+        'n': new_password,
+        't': set_timeout_pwd,
+        'L': lambda vault, item: timeout_loop(vault, locked=True),
+        # actions on items
         'l': list_items,    'ls': list_items,
+        'c': create_item,   'mk': create_item,
         'd': delete_item,   'rm': delete_item,
         'u': load_user,
         'p': load_password,
         'i': load_info,
         'a': load_all,
-        'c': create_item,
-        's': save_vault,
-        'n': new_password,
     }
     user_input = user_input.split()
     cmd = user_input[0]
@@ -396,16 +426,33 @@ def execute_command(vault, user_input):
     return interface[cmd](vault, item)
 
 
-def timeout_loop(vault):
-    clear_screen(msg=False)
-    print(' ** litevault timed out **')
-    while True:
-        pwd = getpass.getpass("Enter password: ")
-        if pwd == vault.password:
+def timeout_loop(vault, locked=False):
+    global curpass
+    curpass = ''
+    clear_screen(False)
+    if locked:
+        print(" ** litevault locked. Buffered password cleared    **")
+    else:
+        print(' ** litevault timed out. Buffered password cleared **')
+    print(" ** 's' will still save and 'q' will quit          ** ")
+    timeout_pwd = vault.get(TIMEOUT_PWD_KEY)
+    msg = "Enter vault password: " if not timeout_pwd else "Enter vault or timeout password: "
+    for n in range(3):
+        pwd = getpass.getpass(msg)
+        if pwd == 'q':
+            quit_app()
+        elif pwd == 's':
+            save_vault()
+            continue
+        if pwd == vault.password or pwd == timeout_pwd:
             return
-        time.sleep(3)
-        print("!! Incorrect password")
+        time.sleep(1)
+        print("!! Incorrect password attempt {} out of 3 !!".format(n+1))
+    quit_app("!! Incorrect password 3 times, exiting !!", rc=1)
 
+
+##################################################
+# App Functions
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -416,6 +463,9 @@ def parse_args():
                         help='password file to load. Default is ~/.vault')
     parser.add_argument('-t', '--timeout', default=300, type=float,
                         help='time in seconds before program is locked with no activity (default=300)')
+    parser.add_argument('-k', '--keypress_delay_us', default=10000, type=int,
+                        help='time in micro-seconds to delay between each keypress when entering passwords')
+    parser.add_argument('--test', action='store_true', help='used for testing')
     args = parser.parse_args()
     verify_environment()
     if args.send_stored_pass:
@@ -424,6 +474,8 @@ def parse_args():
         time.sleep(args.wait)
         subprocess.check_output('kill -10 {}'.format(pid).encode(), shell=True)
         sys.exit()
+    global keypress_delay_us
+    keypress_delay_us = args.keypress_delay_us
     path = os.path.abspath(os.path.expanduser(args.file))
     if not os.path.exists(path):
         print("No vault file at {} -- initializing empty vault".format(args.file))
@@ -438,13 +490,17 @@ def parse_args():
 
 
 def main():
-    signal.signal(signal.SIGINT, lambda *a: sys.exit(1))
+    signal.signal(signal.SIGINT, lambda *a: quit_app(" ** Received Cntrl+C, quitting **"))
     args = parse_args()
     vault = Vault(path=args.path, password=args.password)
     signal.signal(signal.SIGUSR1, output_password)
-    print(intro_msg)
     with open(pid_file, 'w') as f:
         f.write(str(os.getpid()))
+    if args.test:
+        load_password(vault, 'hello')
+        output_password(None, None)
+        return
+    print(intro_msg)
     while True:
         print("command: \rcommand: ", end='')
         i, o, e = select.select([sys.stdin], [], [], args.timeout)
